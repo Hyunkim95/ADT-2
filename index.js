12e58a16baff0dd49558c04eedd3276553bc37ea
Hyun Kim <hkim@live.com.au>
2018-03-25 12:47:19 +1100

    revert file

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const log = require('./lib/log')
 
-const { modify } = require('crocks/State')
+const assign = require('crocks/helpers/assign')
+const B = require('crocks/combinators/composeB')
+const K = require('crocks/combinators/constant')
 
-const compose = require('crocks/helpers/compose')
-const concat = require('crocks/pointfree/concat')
+const Pair = require('crocks/Pair')
+const State = require('crocks/State')
+const prop = require('crocks/Maybe/prop')
+const option = require('crocks/pointfree/option')
 
-// toUpper :: String -> String
-const toUpper =
-  x => x.toUpperCase()
+const { modify, get } = State
 
-// exclaim :: String -> String
-const exclaim =
-  concat('!!!')
+// pluckSeed : Integer -> Object -> Integer
+const pluckSeed = 
+  def => B(option(def), prop('seed'))
 
-// yell :: State String ()
-const yell = modify(
-  compose(exclaim, toUpper)
-)
+// rando : Integer -> State Object Float 
+const rando = s => {
+  const seed = (1103515245 * s + 12345) & 0x7fffffff
+  const value = (seed >>> 16) / 0x7fff
 
-//=> 'NICE!!!'
+  return modify(assign({ seed }))
+    .map(K(value))
+}
+
+// pullRandom : Integer -> () -> State Object Float
+const pullRandom = defSeed => 
+  get(pluckSeed(defSeed)).chain(rando)
+
+// limitIndx : Integer -> Float -> Integer
+const limitIndx =
+  len => x => (x * len)  | 0  
+
+const seed = 76
 
 log( 
-  yell
-    .runWith('nice')
+  State.of(seed)
+    .chain(pullRandom)    
+    .map(limitIndx(52))
+    .runWith({ seed: 23 })
 )
